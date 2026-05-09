1105f6eb8bfd6f905e60b8270c5e7d3b90b944f7
YinDongfang <1136005348@qq.com>
2026-05-09 20:57:44 +0800

    test

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ echo "root: $root"
 echo "target: $target"
 
 tmux kill-session -t "$target" 2>/dev/null || true
-tmux new-session -s "$target" bash -c 'cd "$0" && python3 main.py "$1"; bash' "$root" "$target"
+tmux new-session -s "$target" bash -c 'cd "$0" && python3 main.py "$1" --session="$target"; bash' "$root" "$target"
